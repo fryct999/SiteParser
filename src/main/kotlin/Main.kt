ef52f7a6d540af524d4612595dc810127aa0a7d9
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-11-03 05:30:34 +0300

    SP add conspect for video

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,22 @@
 package org.example
 
-fun main() {
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
 
+fun main() {
+    val doc: Document = Jsoup.connect("https://en.wikipedia.org/").get()
+    //println(doc.title())
+
+    // #id - поиск по id
+    // .classname - поиск по классу
+    // a, div - поиск по тегу
+
+    val newsHeadLines: Elements = doc.select("#mp-itn b a")
+    //println(newsHeadLines)
+
+    for (healLine: Element in newsHeadLines) {
+        println("${healLine.attr("title")}\n\t${healLine.attr("href")}")
+    }
 }
